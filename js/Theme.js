0f38447cb195fb1b88ec80eded6d0b7b5b4f071f
jakubLabedowicz <jalabedowicz@gmail.com>
2021-04-11 17:03:12 +0200

    edit

--- js/Theme.js
+++ js/Theme.js
@@ -92,4 +92,7 @@ class ThemeSwitch {
   }
 }
 
-var s = document.createElement("script").src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
