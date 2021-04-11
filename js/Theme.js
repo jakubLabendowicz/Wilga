f43a02d83acc09badf3e5e825c9929d0c468f17c
jakubLabedowicz <jalabedowicz@gmail.com>
2021-04-11 17:40:06 +0200

    edit

--- js/Theme.js
+++ js/Theme.js
@@ -58,7 +58,7 @@ class ThemeSwitch {
   addStatus(id) {
     this.statuses.push(id);
     //tutaj
-    document.getElementById(statuses[statusesCounter]).onclick = this.show();
+    document.getElementById(this.statuses[this.statusesCounter]).onclick = this.show();
     //do tutaj
     this.statusesCounter++;
   }
